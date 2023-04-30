36d2dc5da3a3847bfe2eebab1b5c4580ea984483
Luerl21 <dmyt20042@gmail.com>
2023-05-01 02:01:25 +0300

    some fizes

--- new/js/main.js
+++ new/js/main.js
@@ -8,18 +8,23 @@ var count = Math.floor(Math.random() * videoArray.length);
 document.getElementsByTagName('source')[0].src = "./videos/" + videoArray[count];video.load();}
 var videoArray = ["bluelock.mp4","Blake Basic  BAD INFLUENCE ft Yamakazi.mp4","UNDAGROUND ENEMY.mp4","GHOSTEMANE  Pouya  Stick Out.mp4","PLAYBOI CARTI  LET EM IN.mp4", "6HOST I HOPE YOU CAVE.mp4", "jpn amend.mp4"];
 
-function pauseplay() { 
-    if(video.paused)
-    	video.play();else video.pause();
+function pauseplay(x) {
+	x.classList.toggle("fa-play");
+	x.classList.toggle("fa-pause");
+    if(video.paused) {
+    	video.play();
+	}	
+	else video.pause();
 }
-
 document.addEventListener('DOMContentLoaded', function() {
-	// отслеживаем активность вкладки браузера
+	var x = this.getElementById("pause/play");
 	document.addEventListener('visibilitychange', function() {
-		if (document.hidden) { // ставим видео на паузу
+		if (document.hidden) {
 			video.pause();
-		} else { // продолжаем воспроизведение
+			x.classList.replace("fa-pause", "fa-play");
+		} else {
 			video.play();
+			x.classList.replace("fa-play", "fa-pause");
 		}
 	});
 });
